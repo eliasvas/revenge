59581378afe85c5d78913745f7d49bc53dc5b223
EliasVas <vaselas99@gmail.com>
2019-12-14 20:06:11 +0200

    tweaks

--- Revenge/GameObject.cpp
+++ Revenge/GameObject.cpp
@@ -12,11 +12,12 @@ bool CheckCollision(GameObject* g1, GameObject* g2) {
 	if (g1 == NULL || g2 == NULL)return false;
 	glm::vec3 g1_pos = { g1->transformation_matrix[3][0] + g1->col->offset[0], g1->transformation_matrix[3][1] + g1->col->offset[1], g1->transformation_matrix[3][2] + g1->col->offset[2] };
 	glm::vec3 g2_pos = { g2->transformation_matrix[3][0] + g2->col->offset[0], g2->transformation_matrix[3][1] + g2->col->offset[1], g2->transformation_matrix[3][2] + g2->col->offset[2] };
-	std::cout << "g1_pos = "<< glm::to_string(g1_pos) << ", g2_pos = " << glm::to_string(g2_pos)<< std::endl;
-	if (g1->col->radius > sqrt((g2_pos.x - g1_pos.x)*(g2_pos.x - g1_pos.x)+(g2_pos.y-g1_pos.y)*(g2_pos.y - g1_pos.y)+(g2_pos.z - g1_pos.z)*(g2_pos.z - g1_pos.z))) {
+	float l = glm::length(g2_pos - g1_pos);
+	std::cout << "radius = "<< g1->col->radius << ", l = " << l << std::endl;
+
+	if (g1->col->radius > l) {
 		return true;
 	}
 	return false;
-
 
 }
